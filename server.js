eb342305460b9bd4bd2c22f0447f3defa6a79787
deisypaolaarteaga-alt <deisypaolaarteaga@gmail.com>
2026-02-18 16:48:07 -0500

    primer endpoint

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+
+    if (req.url === "/deisy") {
+
+        const aprendiz = {
+            nombre: "Deisy",
+            formacion: "Análisis y Desarrollo de Software",
+            centro: "SENA"
+        };
+
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        });
+
+        res.end(JSON.stringify(aprendiz));
+
+    } else {
+
+        res.writeHead(404, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(JSON.stringify({ error: "Ruta no encontrada" }));
+    }
+
+}).listen(3000);
+
+console.log("Servidor corriendo en http://localhost:3000");
